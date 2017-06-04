d943c4675df301e1c0f7a86e2e141c27c4f339de
mdlandis <math.landis@gmail.com>
2017-06-04 01:38:40 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,80 @@
+(function() {
+  var $imgs = $('#gallery img');
+  var $buttons = $('#buttons');
+  var tagged = {};
+
+  $imgs.each(function() {
+    var img = this;
+    var tags = $(this).data('tags');
+
+    if (tags) {
+      tags.split(',').forEach(function(tagName) {
+        if (tagged[tagName] == null) {
+          tagged[tagName] = [];
+        }
+        tagged[tagName].push(img);
+      })
+    }
+  })
+
+  $('<button/>', {
+    text: 'Show All',
+    class: 'active',
+    click: function() {
+      $(this)
+        for (i = 0; i < $imgs.length; i++) { 
+          $imgs[i].parentElement.style.display = 'inline';
+        }
+      $imgs.show();
+    }
+  }).appendTo($buttons);
+
+  $.each(tagged, function(tagName) {
+    var $n = $(tagged[tagName]).length;
+    $('<button/>', {
+      text: tagName + '(' + $n + ')',
+      click: function() {
+        $(this)
+          .addClass('active')
+          .siblings()
+          .removeClass('active');
+        for (i = 0; i < $imgs.length; i++) { 
+          $imgs[i].parentElement.style.display = 'none';
+        }
+        var temp = $imgs.filter(tagged[tagName])
+        for (i = 0; i < temp.length; i++) { 
+          temp[i].parentElement.style.display = 'inline';
+        }
+      }
+    }).appendTo($buttons);
+  });
+}())
+
+(function() {
+  var $imgs = $('#gallery img');
+  var $search = $('#filter-search');
+  var cache = [];
+
+  $imgs.each(function() {
+    cache.push({
+      element: this,
+      text: this.alt.trim().toLowerCase()
+    })
+  })
+
+  function filter() {
+    var query = this.value.trim().toLowerCase();
+    cache.forEach(function(img) {
+      var index = 0;
+      if (query) {
+        index = img.text.indexOf(query);
+      }
+      img.element.parentElement.style.display = index === -1 ? 'none' : '';
+    })
+  }
+  if ('oninput' in $search[0]) {
+    $search.on('input', filter);
+  } else {
+    $search.on('keyup', filter);
+  }
+}())
